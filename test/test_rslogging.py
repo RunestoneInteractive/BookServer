7836e8e4f5b6de5ff07eaf65df8f0adab3aa8bd9
Brad Miller <brad@runestoneinteractive.com>
2020-12-15 10:18:11 -0600

    Simple tests

---
+++ test/test_rslogging.py
@@ -0,0 +1,26 @@
+# This file is for testing the logging API
+# simply run `pytest` fastapi takes care of starting up a server!
+#
+from fastapi.testclient import TestClient
+from app.schemas import LogItemIncoming
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_main():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_add_log():
+    item = LogItemIncoming(
+        event="page", act="view", div_id="/runestone/fopp/index.html"
+    )
+    response = client.post(
+        "/logger/bookevent",
+        headers={"Content-type": "application/json; charset=utf-8"},
+        json=item.dict(),
+    )
+    assert response.status_code == 200
+    assert response.json()["status"] == "OK"
