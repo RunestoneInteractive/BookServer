4a779325b96d1686a160cdb04be82bc3546b16f4
Brad Miller <bonelake@mac.com>
2020-12-16 14:34:49 -0600

    Serving all pages and assets now.

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,17 +1,23 @@
 #
 # Serve book pages from their template
 #
+import logging
+
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
 
 from ..schemas import LogItem, LogItemIncoming
 from ..crud import create_useinfo_entry
 from ..database import get_db
 from config import settings
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 #
 # Setup the router object for the endpoints defined in this file.  These will
@@ -22,14 +28,36 @@ router = APIRouter(
     tags=["books"],  # groups all logger tags together in the docs
 )
 
+# Static Asset renderer
+# https://fastapi.tiangolo.com/tutorial/static-files/?h=+staticfiles#use-staticfiles
+# https://fastapi.tiangolo.com/advanced/custom-response/?h=+filere#fileresponse
+# for paths like: /books/published/basecourse/_static/rest
+# If it is fast and efficient to handle it here it would be great.  We currently avoid
+# any static file contact with web2py and handle static files upstream with nginx directly
+# Note the use of the path type for filepath in the decoration.  If you don't use path it
+# seems to only get you the `next` part of the path /pre/vious/next/the/rest
+@router.get("/published/{course:str}/_static/{filepath:path}")
+async def get_static(course:str, filepath:str):
+    filepath = f"/Users/bmiller/Runestone/{course}/build/{course}/_static/{filepath}"
+    logger.debug(f"GETTING: {filepath}")
+    return FileResponse(filepath)
+    
+@router.get("/published/{course:str}/_images/{filepath:path}")
+async def get_image(course:str, filepath:str):
+    filepath = f"/Users/bmiller/Runestone/{course}/build/{course}/_images/{filepath}"
+    logger.debug(f"GETTING: {filepath}")
+    return FileResponse(filepath)
 
+# Basic page renderer
+# http://localhost:8080/books/published/overview/index.html
+# https://fastapi.tiangolo.com/advanced/templates/?h=+template
 @router.get(
-    "/published/{course:str}/{chapter:str}/{subchapter:str}",
+    "/published/{course:str}/{pagepath:path}",
     response_class=HTMLResponse,
 )
-async def serve_page(request: Request, course: str, chapter: str, subchapter: str):
+async def serve_page(request: Request, course: str, pagepath: str):
     templates = Jinja2Templates(
-        directory=f"/Users/bmiller/Runestone/{course}/build/{course}/{chapter}"
+        directory=f"/Users/bmiller/Runestone/{course}/build/{course}"
     )
     # request.application -- NA for FastAPI
     # course_name
@@ -40,7 +68,6 @@ async def serve_page(request: Request, course: str, chapter: str, subchapter: st
     # allow_pairs
     # activity_info
     # settings.google_ga
-    print(course, chapter, subchapter)
     context = dict(
         request=request,
         course_name=course,
@@ -51,5 +78,8 @@ async def serve_page(request: Request, course: str, chapter: str, subchapter: st
         allow_pairs="false",
         activity_info={},
         settings=settings,
+        is_logged_in="false",
+        is_instructor="true",
+        readings = [],
     )
-    return templates.TemplateResponse(subchapter, context)
+    return templates.TemplateResponse(pagepath, context)
