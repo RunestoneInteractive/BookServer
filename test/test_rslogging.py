a211b3eec5b810d48af949adf170998c07619454
Brad Miller <bonelake@mac.com>
2021-04-26 18:33:13 -0500

    Fix make sure that startup and shutdown run

--- test/test_rslogging.py
+++ test/test_rslogging.py
@@ -6,12 +6,11 @@ from fastapi.testclient import TestClient
 from bookserver.schemas import LogItemIncoming
 from bookserver.main import app
 
-client = TestClient(app)
-
 
 def test_main():
-    response = client.get("/")
-    assert response.status_code == 200
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
 
 
 def test_add_log():
@@ -22,10 +21,11 @@ def test_add_log():
         sid="testuser",
         course_name="fopp",
     )
-    response = client.post(
-        "/logger/bookevent",
-        headers={"Content-type": "application/json; charset=utf-8"},
-        json=item.dict(),
-    )
-    assert response.status_code == 200
-    assert response.json()["status"] == "OK"
+    with TestClient(app) as client:
+        response = client.post(
+            "/logger/bookevent",
+            headers={"Content-type": "application/json; charset=utf-8"},
+            json=item.dict(),
+        )
+        assert response.status_code == 200
+        assert response.json()["status"] == "OK"
