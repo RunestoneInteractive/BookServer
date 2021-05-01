ddb9e97cc26e4b228206a294469b149c20626c6d
bjones1 <bjones@ece.msstate.edu>
2021-05-01 18:36:42 +0000

    Add: Convenient access to SQLAlchemy Core.

--- bookserver/models.py
+++ bookserver/models.py
@@ -102,6 +102,11 @@ def register_answer_table(cls):
 class IdMixin:
     id = Column(Integer, primary_key=True)
 
+    @classmethod
+    @property
+    def t(cls):
+        return cls.__table__
+
 
 # Useinfo
 # -------
