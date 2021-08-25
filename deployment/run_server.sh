00c3ba0ea7c25613e10102798c1c82d0a1baec86
bjones1 <bjones@ece.msstate.edu>
2021-08-25 12:46:19 +0100

    WIP 8

--- deployment/run_server.sh
+++ deployment/run_server.sh
@@ -9,14 +9,24 @@
 # - This venv also contains all the web2py pip dependencies.
 # - The web2py referred to below has ``wsgihandler.py`` in its root directory (you can copy this from ``web2py/handlers``).
 #
+# This file must be manually edited to provide various settings.
+#
 # Start servers
 # =============
 # Echo all commands for debug. Comment out to reduce noise.
 set -x
 
+# Edit me: Specify the absolute path to web2py.
+WEB2PY_PATH=$PWD/../../web2py
+# Edit me: Specify the Postgresql URL.
+POSTGRESQL_URL=runestone:bully@localhost/runestone
+
+# The nginx config doesn't allow env vars. Do some specific substitution (don't substitute everything, since this would replace nginx vars like $host). From `SO <https://serverfault.com/a/919212>`_.
+SITE_NAME=127.0.0.1 WEB2PY_PATH=$WEB2PY_PATH envsubst '${SITE_NAME} ${WEB2PY_PATH}' < $PWD/nginx.conf > /tmp/nginx.conf
+
 # The user-facing web server. Try stopping it first (which produces an error if it's not started.)
 sudo nginx -s stop
-sudo nginx -c $PWD/nginx.conf
+sudo nginx -c /tmp/nginx.conf
 
 # See `stopping the server <https://uwsgi-docs.readthedocs.io/en/latest/Management.html#stopping-the-server>`_.
 sudo env "PATH=$PATH" uwsgi --stop /tmp/uwsgi.pid
@@ -25,13 +35,13 @@ sudo env \
     "PATH=$PATH" \
     WEB2PY_CONFIG=production \
     WEB2PY_MIGRATE=Yes \
-    DBURL=postgresql://runestone:bully@localhost/runestone \
-    uwsgi --ini $PWD/uwsgi.ini --chdir=$PWD/../../web2py &
+    DBURL=postgresql://POSTGRESQL_URL \
+    uwsgi --ini $PWD/uwsgi.ini --chdir=$WEB2PY_PATH &
 
 sudo pkill gunicorn
 # This runs the BookServer.
 sudo env \
     "PATH=$PATH" \
-    DBURL=postgresql+asyncpg://runestone:bully@localhost/runestone \
+    DBURL=postgresql+asyncpg://$POSTGRESQL_URL \
     BOOK_SERVER_CONFIG=production \
     gunicorn --config $PWD/gunicorn.conf.py --bind=unix:/run/gunicorn.sock &
