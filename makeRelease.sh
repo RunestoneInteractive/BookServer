72f17ebec953d62e0b0129aa2194ef22055fe4af
Brad Miller <bonelake@mac.com>
2022-08-10 10:51:28 -0500

    make a release

---
+++ makeRelease.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage:  makeRelease <release no>"
+    exit
+fi
+
+while true; do
+read -p "Did you update/commit the version in pyproject.toml " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "building python package"
+poetry build
+poetry publish
+ 
+echo "tagging this release and pushing to github"
+
+git tag -a $1 -m 'tag new version'
+git push --follow-tags
+gh release create v$1 --generate-notes
